6cc8628f6ee79c9bff0c11984265256ff63f3cad
DonatoVerardi <donato.verardi@gmail.com>
2020-12-27 17:35:34 +0100

    final version

--- main.py
+++ main.py
@@ -8,39 +8,27 @@ pygame.font.init()
 
 # Game parameters
 NB_BLOCK_X, NB_BLOCK_Y = 20, 20
-BLOCK_SIZE = 20
-FPS = 4
+BLOCK_SIZE = 40
+FPS = 1
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
 RED = (255, 0, 0)
 SILVER = (192, 192, 192)
-GRID_COLOR = (62, 62, 62)
-GAME_OVER_FONT = pygame.font.SysFont("comicsans", 80)
-GAME_OVER_FONT.bold = True
+BG_COLOR = (204, 255, 153)
+GRID_COLOR = (229, 255, 204)
+GAME_OVER_FONT_BOLD = pygame.font.SysFont("comicsans", 80)
+GAME_OVER_FONT_BOLD.bold = True
 GAME_OVER_FONT_SPACE = pygame.font.SysFont("comicsans", 20)
 
-class Food:
 
-    def __init__(self):
-        self.x = 0
-        self.y = 0
-        self.food_surface = pygame.Surface((BLOCK_SIZE, BLOCK_SIZE))
-        self.food_surface.fill(RED)
+APPLE_IMG = pygame.transform.scale(pygame.image.load(os.path.join(
+    "assets", "apple.png")), (BLOCK_SIZE, BLOCK_SIZE))
 
-    def draw_food(self, screen):
-        food_rect = (self.food_surface).get_rect(
-            topleft=(self.x * BLOCK_SIZE, self.y * BLOCK_SIZE))
-        screen.blit(self.food_surface, food_rect)
+SNAKE_BODY_IMG = pygame.transform.scale(pygame.image.load(os.path.join(
+    "assets", "snake_body.png")), (BLOCK_SIZE, BLOCK_SIZE))
 
-    def generate_food(self, grid, snake):
-        food_grid = []
-        for i in grid.grid_tuples:
-            if i not in snake.body:
-                food_grid.append(i)
-
-        position = random.choice(food_grid)
-        self.x = position[0]
-        self.y = position[1]
+SNAKE_HEAD_IMG = pygame.transform.scale(pygame.image.load(os.path.join(
+    "assets", "snake_head.png")), (BLOCK_SIZE, BLOCK_SIZE))
 
 
 class Grid:
@@ -59,6 +47,30 @@ class Grid:
         for square in self.grid_rect:
             pygame.draw.rect(screen, GRID_COLOR, square, 1)
 
+class Food:
+
+    def __init__(self):
+        self.x = 0
+        self.y = 0
+        #self.food_surface = pygame.Surface((BLOCK_SIZE, BLOCK_SIZE))
+        # self.food_surface.fill(RED)
+        self.food_surface = APPLE_IMG.convert_alpha()
+
+    def draw_food(self, screen):
+        food_rect = (self.food_surface).get_rect(
+            topleft=(self.x * BLOCK_SIZE, self.y * BLOCK_SIZE))
+        screen.blit(self.food_surface, food_rect)
+
+    def generate_food(self, grid, snake):
+        food_grid = []
+        for i in grid.grid_tuples:
+            if i not in snake.body:
+                food_grid.append(i)
+
+        position = random.choice(food_grid)
+        self.x = position[0]
+        self.y = position[1]
+
 
 class Snake:
 
@@ -68,6 +80,8 @@ class Snake:
         self.snake_surface = pygame.Surface((BLOCK_SIZE, BLOCK_SIZE))
         self.snake_surface.fill(WHITE)
         self.grow = False
+        self.head_surface = SNAKE_HEAD_IMG.convert_alpha()
+        self.body_surface = SNAKE_BODY_IMG.convert_alpha()
 
  # left: l, right: r, up: u, down: d
     def move(self, direction):
@@ -81,35 +95,59 @@ class Snake:
         else:
             self.body.pop()
 
-    def draw_snake(self, screen):
-        for tail in self.body:
+    def draw_snake(self, screen, last_move):
+
+        head_rect = (self.head_surface).get_rect(
+            topleft=(self.head[0] * BLOCK_SIZE, self.head[1] * BLOCK_SIZE))
+
+        head_surface_rotate = self.head_surface
+
+        # go left
+        if last_move == (-1, 0):
+            head_surface_rotate = pygame.transform.rotate(
+                self.head_surface, -90)
+        # go right
+        elif last_move == (1, 0):
+            head_surface_rotate = pygame.transform.rotate(
+                self.head_surface, 90)
+        # go up
+        elif last_move == (0, -1):
+            head_surface_rotate = pygame.transform.rotate(
+                self.head_surface, 180)
+
+        screen.blit(head_surface_rotate, head_rect)
+
+        for tail in self.body[1:]:
             x = tail[0]
             y = tail[1]
-            # pygame.draw.rect(screen, RED,  pygame.Rect(tail[0] * BLOCK_SIZE, tail[1] * BLOCK_SIZE, BLOCK_SIZE, BLOCK_SIZE), 1)
-            snake_rect = (self.snake_surface).get_rect(
+            # snake_rect = (self.snake_surface).get_rect(
+            #    topleft=(x * BLOCK_SIZE, y * BLOCK_SIZE))
+            #screen.blit(self.snake_surface, snake_rect)
+            body_rect = (self.body_surface).get_rect(
                 topleft=(x * BLOCK_SIZE, y * BLOCK_SIZE))
-            screen.blit(self.snake_surface, snake_rect)
+            screen.blit(self.body_surface, body_rect)
 
     def eat_food(self, food):
         if (food.x, food.y) == self.head:
             self.grow = True
 
 
-def draw_window(screen, grid, snake, food, game_over):
+def draw_window(screen, grid, snake, food, game_over, last_move):
 
-    screen.fill((0, 0, 0))
+    screen.fill(BG_COLOR)
     # draw_grid(screen)
     grid.draw_grid(screen)
     food.draw_food(screen)
-    snake.draw_snake(screen)
+    snake.draw_snake(screen, last_move)
 
     if game_over:
-        game_over_text = GAME_OVER_FONT.render("GAME OVER", 1, WHITE)
+
+        game_over_text_bold = GAME_OVER_FONT_BOLD.render("GAME OVER", 1, WHITE)
         space_text = GAME_OVER_FONT_SPACE.render(
             "Press the space bar to start again", 1, WHITE)
 
-        screen.blit(game_over_text, ((NB_BLOCK_X * BLOCK_SIZE / 2) -
-                                     (game_over_text.get_width() / 2), (NB_BLOCK_Y * BLOCK_SIZE / 2) - (game_over_text.get_height() / 2)))
+        screen.blit(game_over_text_bold, ((NB_BLOCK_X * BLOCK_SIZE / 2) -
+                                          (game_over_text_bold.get_width() / 2), (NB_BLOCK_Y * BLOCK_SIZE / 2) - (game_over_text_bold.get_height() / 2)))
 
         screen.blit(space_text, ((NB_BLOCK_X * BLOCK_SIZE / 2) -
                                  (space_text.get_width() / 2), (NB_BLOCK_Y * BLOCK_SIZE) - 20))
@@ -183,7 +221,7 @@ def main():
         if snake.head in snake.body[1:]:
             game_over = True
 
-        draw_window(screen, grid, snake, food, game_over)
+        draw_window(screen, grid, snake, food, game_over, last_move)
 
 
 main()
